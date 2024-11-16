91112ea80d3d4ca8ee73705d9bcc21e2b4df7428
DangDuc1523 <dangduc504@gmail.com>
2024-11-16 22:00:12 +0700

    new  src

--- Solution1/SchoolManagement.Models/obj/Debug/net6.0/SchoolManagement.Models.AssemblyInfo.cs
+++ Solution1/SchoolManagement.Models/obj/Debug/net6.0/SchoolManagement.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593cc0260ea48265e27f193c2a9a333416c9ecf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc6456675c506e9fb5591c523630dd71854bd17")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
